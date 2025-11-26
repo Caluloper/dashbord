357b147a9baee3122ff8c999355beebbe7e86700
Caluloper <carlosluislopezcba@gmail.com>
2025-11-26 19:18:11 +0000

    guia13

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
   return (
     <Grid container spacing={5} justifyContent="center" alignItems="center">
 
       {/* Encabezado */}
-      <Grid size={{ xs:12 , md:12 }}>Elemento: Encabezado</Grid>
+      <Grid size={ 12 }>
+        <HeaderUI />
+      </Grid>
 
       {/* Alertas */}
-      <Grid size={{ xs: 12, md: 12}}>Elemento: Alertas</Grid>
+      <Grid size={ 12 } container justifyContent="right" alignItems="center">
+        <AlertUI description="No se preveen lluvias"/>
+      </Grid>
 
       {/* Selector */}
       <Grid size={{ xs: 12, md: 3}}>Elemento: Selector</Grid>
@@ -30,7 +36,7 @@ function App() {
       </Grid>
 
       {/* Información adicional */}
-      <Grid size={{ xs: 12, md: 12}}>Elemento: Información adicional</Grid>
+      <Grid size={ 12 }>Elemento: Información adicional</Grid>
 
     </Grid>
   )
